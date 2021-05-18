6e5a2582ba2aeb79270b8f67ec7bfb1980ad93b8
Alisha Anand <55910525+anandalisha@users.noreply.github.com>
2021-05-18 19:26:48 +0530

    update view.py

--- user/views.py
+++ user/views.py
@@ -6,12 +6,37 @@ from.forms import SignUpForm
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def home(request):
     return HttpResponse("User app contains anything related to user signup,login etc")
 
 #profile page request
 def profile(request):
     return render(request,"user/profile.html")
+
+def login_user(request):
+    if request.method == "POST":
+        # if someone fills out form , Post it
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # if user exist
+            login(request, user)
+            messages.success(request, ("Youre logged in"))
+            return redirect("home")
+            # routes to 'home' on successful login
+        else:
+            messages.success(request, ("Error logging in"))
+            return redirect("login")
+            # re routes to login page upon unsucessful login
+    else:
+        return render(request, "user/login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Youre now logged out"))
+    return redirect("home")
 
 #to view register form 
 def register(request):
