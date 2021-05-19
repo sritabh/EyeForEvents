686e3450a12d3ada047194b1e01a4df6b9bdce0d
SobyDamn <sobydanny@gmail.com>
2021-05-19 16:03:20 +0530

    Fixed Form Validator for Created_by field

--- events/forms.py
+++ events/forms.py
@@ -6,7 +6,7 @@ from .models import Participant
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = "__all__"
+        fields = ['name', 'description', 'max_capacity','location','contact_number','alt_contact_number','contact_email']
 
 
 class ParticipantForm(forms.ModelForm):
