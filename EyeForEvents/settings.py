42f746d21ae4b1953352bef6f1b1c2af53be433c
SobyDamn <43126956+SobyDamn@users.noreply.github.com>
2021-05-24 16:46:10 +0530

    deployment env added

--- EyeForEvents/settings.py
+++ EyeForEvents/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import secretFile #File containing value of SECRET_KEY
+#import secretFile #File containing value of SECRET_KEY
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secretFile.key_value #ADD Secret key from secret
+SECRET_KEY = os.environ('SECRET_KEY') #ADD Secret key from secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
