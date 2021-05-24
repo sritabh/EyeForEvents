e1769b626623cb33c2bc45014ca9aa51cbd39f2b
SobyDamn <43126956+SobyDamn@users.noreply.github.com>
2021-05-24 17:56:42 +0530

    Path location fixed

--- EyeForEvents/settings.py
+++ EyeForEvents/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'EyeForEvents.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Password validation
