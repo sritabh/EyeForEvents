a65e3ce8497eee8c4f460875ae618e869b47eb9d
SobyDamn <43126956+SobyDamn@users.noreply.github.com>
2021-05-24 17:50:24 +0530

    BAS_DIR fixed

--- EyeForEvents/settings.py
+++ EyeForEvents/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
